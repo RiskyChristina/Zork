979ece944d10d37463e35b00ca348188dc7b1d19
RiskyChristina <cleskowyak2@yahoo.com>
2020-09-10 18:53:58 -0400

    Zork 1.3

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,98 +1,58 @@
 ﻿using System;
-using System.ComponentModel.Design;
-using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Zork
 {
-    enum Commands
-    {
-        QUIT,
-        LOOK,
-        NORTH,
-        SOUTH,
-        EAST,
-        WEST,
-        UNKNOWN
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-           // Commands command = ToCommand(inputString.Trim().ToUpper());
-           // Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write(">");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                String outputString;
+
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+
+                    case Commands.LOOK:
+                        outputString = "There is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+
+                    case Commands.NORTH:
+                        outputString = "You moved North";
+                        break;
+
+                    case Commands.SOUTH:
+                        outputString = "You moved South";
+                        break;
+
+                    case Commands.WEST:
+                        outputString = "You moved West";
+                        break;
+
+                    case Commands.EAST:
+                        outputString = "You moved East";
+                        break;
+
+                    default:
+                        outputString = "Unknown command";
+                        break;
+                }
+               
+                Console.WriteLine(outputString);
+            }
+
         }
 
-        private static Commands ToCommand(string commandString)
-        {
-            Commands command;
-            switch (commandString)
-            {
-                case "QUIT":
-                    command = Commands.QUIT;
-                    break;
-
-                case "LOOK":
-                    command = Commands.LOOK;
-                    break;
-
-                case "NORTH":
-                    command = Commands.NORTH;
-                    break;
-
-                case "SOUTH":
-                    command = Commands.SOUTH;
-                    break;
-
-                case "EAST":
-                    command = Commands.EAST;
-                    break;
-
-                case "WEST":
-                    command = Commands.WEST;
-                    break;
-
-                default:
-                    command = Commands.UNKOWN;
-                    break;
-            };
-
-            return command;
-
-           /* if (commandString == "QUIT")
-            {
-                command = Commands.QUIT;
-            }
-            else if (commandString == "LOOK")
-            {
-                command = Commands.LOOK;
-            }
-            else if (commandString == "NORTH")
-            {
-                command = Commands.NORTH;
-            }
-            else if (commandString == "SOUTH")
-            {
-                command = Commands.SOUTH;
-            }
-            else if (commandString == "EAST")
-            {
-                command = Commands.EAST;
-            }
-            else if (commandString == "WEST")
-            {
-                command = Commands.WEST;
-            }
-            else
-            {
-                command = Commands.UNKNOWN;
-            }
-
-            return command;*/
-        }
+        private static Commands ToCommand(string inputString) => Enum.TryParse(inputString, true, out Commands result) ? result : Commands.UNKNOWN;
     }
 }
-         
