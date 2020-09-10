499dca81a3700fa3cddba6756ffde1f842602acb
RiskyChristina <cleskowyak2@yahoo.com>
2020-09-10 17:53:15 -0400

    Zork 1.2

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,7 +1,20 @@
 ﻿using System;
+using System.ComponentModel.Design;
+using System.Linq;
 
 namespace Zork
 {
+    enum Commands
+    {
+        QUIT,
+        LOOK,
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+        UNKNOWN
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -9,19 +22,77 @@ namespace Zork
             Console.WriteLine("Welcome to Zork!");
 
             string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
-            if (inputString == "QUIT")
+           // Commands command = ToCommand(inputString.Trim().ToUpper());
+           // Console.WriteLine(command);
+        }
+
+        private static Commands ToCommand(string commandString)
+        {
+            Commands command;
+            switch (commandString)
             {
-                Console.WriteLine("Thank you for playing.");
+                case "QUIT":
+                    command = Commands.QUIT;
+                    break;
+
+                case "LOOK":
+                    command = Commands.LOOK;
+                    break;
+
+                case "NORTH":
+                    command = Commands.NORTH;
+                    break;
+
+                case "SOUTH":
+                    command = Commands.SOUTH;
+                    break;
+
+                case "EAST":
+                    command = Commands.EAST;
+                    break;
+
+                case "WEST":
+                    command = Commands.WEST;
+                    break;
+
+                default:
+                    command = Commands.UNKOWN;
+                    break;
+            };
+
+            return command;
+
+           /* if (commandString == "QUIT")
+            {
+                command = Commands.QUIT;
             }
-            else if (inputString == "LOOK")
+            else if (commandString == "LOOK")
             {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+                command = Commands.LOOK;
+            }
+            else if (commandString == "NORTH")
+            {
+                command = Commands.NORTH;
+            }
+            else if (commandString == "SOUTH")
+            {
+                command = Commands.SOUTH;
+            }
+            else if (commandString == "EAST")
+            {
+                command = Commands.EAST;
+            }
+            else if (commandString == "WEST")
+            {
+                command = Commands.WEST;
             }
             else
             {
-                Console.WriteLine("Unrecognized command.");
+                command = Commands.UNKNOWN;
             }
+
+            return command;*/
         }
     }
 }
+         
