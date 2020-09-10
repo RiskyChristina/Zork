fde630bfae1aceb06615e0fb9ea30336043b44fe
RiskyChristina <cleskowyak2@yahoo.com>
2020-09-10 19:04:31 -0400

    Zork 1.2
    other 1.2 was not the correct save

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -9,47 +9,9 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            Commands command = Commands.UNKNOWN;
-            while (command != Commands.QUIT)
-            {
-                Console.Write(">");
-                command = ToCommand(Console.ReadLine().Trim());
-
-                String outputString;
-
-                switch (command)
-                {
-                    case Commands.QUIT:
-                        outputString = "Thank you for playing!";
-                        break;
-
-                    case Commands.LOOK:
-                        outputString = "There is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
-                        break;
-
-                    case Commands.NORTH:
-                        outputString = "You moved North";
-                        break;
-
-                    case Commands.SOUTH:
-                        outputString = "You moved South";
-                        break;
-
-                    case Commands.WEST:
-                        outputString = "You moved West";
-                        break;
-
-                    case Commands.EAST:
-                        outputString = "You moved East";
-                        break;
-
-                    default:
-                        outputString = "Unknown command";
-                        break;
-                }
-               
-                Console.WriteLine(outputString);
-            }
+            string inputString = Console.ReadLine();
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
 
         }
 
